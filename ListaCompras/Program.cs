01f9e562648b8cb34670a77f6a7825160ad83d59
Arthur <117952514+Arthur-zct@users.noreply.github.com>
2025-10-21 19:18:20 -0300

    Estrutura

--- ListaCompras/Program.cs
+++ ListaCompras/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace listacompras;
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string path = @"C:\Users\Arthur\Documents\c#\Gerenciador_de_compras\ListaCompras";
+        string fileName = "ListaCompras.txt";
+        string filePath = path + fileName;
+
+        List<string> files = new List<string>();
+
+
+    }
+}
