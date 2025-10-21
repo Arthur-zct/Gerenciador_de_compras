e41b293df57e63a7a3cf2e14898da0438f5d0779
Arthur <117952514+Arthur-zct@users.noreply.github.com>
2025-10-21 20:49:18 -0300

    Funções criadas e funcionando
    Adiciona e edita os itens pretendidos corretamente

--- ListaCompras/Program.cs
+++ ListaCompras/Program.cs
@@ -5,12 +5,81 @@ public class Program
 {
     public static void Main()
     {
-        string path = @"C:\Users\Arthur\Documents\c#\Gerenciador_de_compras\ListaCompras";
-        string fileName = "ListaCompras.txt";
+        string path = @"C:\Users\Arthur\Documents\c#\Gerenciador_de_compras\ListaCompras\";
+        string fileName = "ListaDeCompras.txt";
         string filePath = path + fileName;
 
-        List<string> files = new List<string>();
+        List<string> shoopingList = new List<string>();
 
 
+        if ( File.Exists(filePath))
+        {
+            shoopingList.AddRange(File.ReadAllLines(filePath));
+        }
+
+        while (true)
+        {
+            Console.WriteLine("\n === Lista de compras ===");
+            Console.WriteLine("1. Adicionar item");
+            Console.WriteLine("2. Remover item");
+            Console.WriteLine("3. Exibir lista");
+            Console.WriteLine("4. Exportar lista em .txt");
+            Console.Write("Escolha uma opção: ");
+            string choiceUser = Console.ReadLine();
+
+            switch(choiceUser)
+            {
+                case "1":
+                    Console.Write("Digite o nome do item para adicionar: ");
+                    string itemInsert = Console.ReadLine();
+                    if (!string.IsNullOrEmpty(itemInsert))
+                    {
+                        shoopingList.Add(itemInsert);
+                        Console.WriteLine($"Item '{itemInsert}' adicionado à lista.");
+                    } 
+                    else
+                    {
+                        Console.WriteLine("Item inválido. Tente novamente.");
+                    }
+                    break;
+
+                case "2": 
+                    Console.WriteLine("Digite o nome do item para remover: ");
+                    string itemToRemove = Console.ReadLine();
+                    if( shoopingList.Remove(itemToRemove) )
+                    {
+                        Console.WriteLine($"Item '{itemToRemove}' removido da lista.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Item '{itemToRemove}' não encontrado.");
+                    }
+                break;
+
+                case "3":
+                    Console.WriteLine("\n Itens na sua lista de compras: ");
+                    if(shoopingList.Count ==0)
+                    {
+                        Console.WriteLine("Sua lista de compras está vazia.");
+                    }
+                    else
+                    {
+                        for(int i = 0; i < shoopingList.Count; i++)
+                        {
+                            Console.WriteLine($"{i + 1}. {shoopingList[i]}");
+                        }
+                    }
+                break;
+
+                case "4":
+                    File.WriteAllLines(filePath, shoopingList);
+                    Console.WriteLine("Lista salva. Saindo do programa...");
+                    return;
+
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                break;
+            }
+        }
     }
 }
